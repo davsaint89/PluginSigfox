0c94d17a3561b03d1aa0dd547c7b5fcaff04449b
davsaint89 <61285898+davsaint89@users.noreply.github.com>
2021-08-18 22:30:10 -0500

    ubidots_request added

--- src/function.py
+++ src/function.py
@@ -1,11 +1,36 @@
 import requests
 import time
+import json
 from datetime import datetime
 from dateutil.parser import parse
 
-BASE_URL = 'https://industrial.api.ubidots.com'
+BASE_URL = 'https://industrial.api.ubidots.com/api/v1.6/devices/'
 VERSION = 'v1.6'
 
 def main(args):
+
     if not 'token' in args['_parameters']:
-        print("[ERROR]")
+        print("[ERROR] Ubidots token not specified")
+        return {"status":"error"}
+    else:
+        print("[INFO] args:", json.dumps(args))
+    
+        token = args['token']
+        device_label = "demo_sigfox"
+        payload =json.dumps(args)
+        response = ubidots_request(device_label, payload, token)
+        return response
+    
+
+def ubidots_request(device_label, payload, token):
+    BASE_URL = 'https://industrial.api.ubidots.com/api/v1.6/devices/'
+
+    headers = {
+        "X-Auth-Token": token,
+    "Content-Type": "application/json",
+    }
+    response = requests.post(BASE_URL+device_label, headers=headers, data = payload)
+    return response
+
+
+
