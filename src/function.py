03c15e5aa0e61bd5eb271928c431514e9bd8d3dd
davsaint89 <61285898+davsaint89@users.noreply.github.com>
2021-08-19 18:32:48 -0500

    _params fixed

--- src/function.py
+++ src/function.py
@@ -1,25 +1,25 @@
 import requests
-import time
 import json
-from datetime import datetime
-from dateutil.parser import parse
 
-BASE_URL = 'https://industrial.api.ubidots.com/api/v1.6/devices/'
-VERSION = 'v1.6'
 
 def main(args):
 
-    if not 'token' in args['_parameters']:
+    if not args['_parameters'].get('token'):
         print("[ERROR] Ubidots token not specified")
         return {"status":"error"}
     else:
         print("[INFO] args:", json.dumps(args))
     
-        token = args['token']
-        device_label = "demo_sigfox"
+        token = args['_parameters']['token']
+        args.pop('_parameters')
+        #args.pop("token")
+        device_label = args['device_id']
+        args.pop("device_id")
         payload =json.dumps(args)
+        #print(payload)
         response = ubidots_request(device_label, payload, token)
-        return response
+        print(response, response.json())
+        return response.json()
     
 
 def ubidots_request(device_label, payload, token):
@@ -27,7 +27,7 @@ def ubidots_request(device_label, payload, token):
 
     headers = {
         "X-Auth-Token": token,
-    "Content-Type": "application/json",
+        "Content-Type": "application/json",
     }
     response = requests.post(BASE_URL+device_label, headers=headers, data = payload)
     return response
