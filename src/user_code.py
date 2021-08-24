70ae8963ca904c9d33b95ca320f9ba831a18fdf3
davsaint89 <61285898+davsaint89@users.noreply.github.com>
2021-08-24 17:09:47 -0500

    user_functions updated

--- src/user_code.py
+++ src/user_code.py
@@ -11,15 +11,7 @@ def decode_data(hex_array):
     Put your decoding function here.
     We recommend using Python struct package or bitwise operations 
     to operate the binary data
-    '<f' for little endian
-    '>f' for big endian
-    4 bytesminimum requiered for unpacking
+
     '''
-    temp = int(hex_array[0])
-    lat = round(struct.unpack('>f', hex_array[1:5])[0],2) 
-    ln = round(struct.unpack('>f', hex_array[5:10])[0],2) 
-
-    payload['temperatura'] = {'value': temp, 'context':{'lat':lat, 'ln':ln}}
-
     return payload
 
