9e0e5e294e3da1d5b613b0a7aef2c266278df2c6
davsaint89 <61285898+davsaint89@users.noreply.github.com>
2021-09-02 12:47:41 -0500

    function update

--- src/function.py
+++ src/function.py
@@ -41,10 +41,7 @@ def ubidots_request_dev_type(device_label, device_type, payload, token):
     Updates a device under a device type
     """
     url = BASE_URL + '/api/v1.6/devices/' + device_label + '/?type=' + device_type
-    headers = {
-        "X-Auth-Token": token,
-        "Content-Type": "application/json",
-    }
+    headers = {"X-Auth-Token": token,"Content-Type": "application/json"}
     response = create_request(url, headers, attempts=5,request_type='post',data=payload)
     return response
 
@@ -54,10 +51,7 @@ def ubidots_request_device(data, device_label, token):
     Updates a device
     """
     url = BASE_URL + '/api/v1.6/devices/' + device_label
-    headers = {
-        "X-Auth-Token": token,
-        "Content-Type": "application/json",
-    }
+    headers = {"X-Auth-Token": token,"Content-Type": "application/json",}
     response = create_request(url, headers, attempts=5, data=data, request_type='post')
     return response
 
